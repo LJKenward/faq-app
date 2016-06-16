3b401d7e7618214edd939e5b2f22aacc5e01b615
LJ Kenward <ljkrooker@gmail.com>
2016-06-16 19:47:24 +1000

    all nav links point at unique content

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,89 @@
-/* activate sidebar */ 
+/* activate sidebar */ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 $(document).ready(function() {
 
     var billingLink = $('#billingLink');
-    var rightContent = $('#right-content');
+    var installationLink = $('#installationLink');
+    var operationsLink = $('#operationsLink');
+    var accountsLink = $('#accountsLink');
+
+    var qandaContent = $('#qanda-content');
 
     billingLink.click(function (){
       var resultData = [
-        {question: 'is the world round', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
-        {question: 'is the world flat', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
-        {question: 'is the sky blue', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'}
+        { idNum: '0', question: 'is the world round', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+        { idNum: '1', question: 'is the world flat', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+        { idNum: '2', question: 'is the sky blue', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
       ]
 
       var htmlStringArray = resultData.map(function(item){
-        return '<h3>Question:' + item.question + '</h3><p>Answer:' + item.answer + '</p>';
+        return '<h3 id="sec' + item.idNum + '">' + item.question + '</h3><p>' + item.answer + '</p><hr>';
       });
 
       var joinedHTMLString = htmlStringArray.join('');
-          rightContent.html(joinedHTMLString);
+          qandaContent.html(joinedHTMLString);
     });
 
-  //Read specific content from local JSON file and display it
+/* end billing function*/
+
+/* start installation function*/
+
+    installationLink.click(function (){
+      var resultData = [
+        { idNum: '0', question: 'How do I do it?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+        { idNum: '1', question: 'Where can I fix it?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+        { idNum: '2', question: 'When will it go live?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+      ]
+
+      var htmlStringArray = resultData.map(function(item){
+        return '<h3 id="sec' + item.idNum + '">' + item.question + '</h3><p>' + item.answer + '</p><hr>';
+      });
+
+      var joinedHTMLString = htmlStringArray.join('');
+
+          qandaContent.html(joinedHTMLString);
+    });
+
+/* end installationLink function */
+
+/* start Operations function*/
+
+    operationsLink.click(function (){
+      var resultData = [
+        { idNum: '0', question: 'Should do I do it?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+        { idNum: '1', question: 'Can I fix it?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+        { idNum: '2', question: 'Where will it go live?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+      ]
+
+      var htmlStringArray = resultData.map(function(item){
+        return '<h3 id="sec' + item.idNum + '">' + item.question + '</h3><p>' + item.answer + '</p><hr>';
+      });
+
+      var joinedHTMLString = htmlStringArray.join('');
+
+          qandaContent.html(joinedHTMLString);
+    });
+
+/* end operations function*/
+
+/* start accountsLink function*/
+
+    accountsLink.click(function (){
+      var resultData = [
+        { idNum: '0', question: 'How Do I delete?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+        { idNum: '1', question: 'Update my image?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+        { idNum: '2', question: 'Change my username?', answer: 'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.'},
+      ]
+
+      var htmlStringArray = resultData.map(function(item){
+        return '<h3 id="sec' + item.idNum + '">' + item.question + '</h3><p>' + item.answer + '</p><hr>';
+      });
+
+      var joinedHTMLString = htmlStringArray.join('');
+
+          qandaContent.html(joinedHTMLString);
+    });
+
+/* end accountsLink function*/
 
     $('#sidebar').affix({
         offset: {
